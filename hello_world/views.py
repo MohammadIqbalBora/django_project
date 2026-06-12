c9c2a14d3a1ba551f7dabbf2d64a7f01bc00448f
IqbalBora <mi_bora@yahoo.co.uk>
2026-06-12 22:13:57 +0100

    Add inside view

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the hello world index.")
+    # return HttpResponse("Hello, world. You're at the hello world index.")
+     if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
+    
 
